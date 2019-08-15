160f6a11765d0084a6b61e5d4a800189aa716177
Maria Afanaseva <mashenkachukina@gmail.com>
2019-08-15 11:35:54 +0200

    Lesson_5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,51 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+import collections
+
+
+def get_avg():
+    company = collections.namedtuple('Company', 'Name quarter_I quarter_II quarter_III quarter_IV profit_sum')
+    n = int(input('Введите колличество компаний: '))
+    profit_all = 0
+    companies = []
+    for i in range(n):
+        name = input('Введите название компании: ')
+        profit_1, profit_2, profit_3, profit_4 = \
+            list(map(int, input('Введите прибыль за четыре квартала года через пробел: ').split()))
+        profit = profit_1 + profit_2 + profit_3 + profit_4
+        profit_all += profit
+        company_i = company(Name=name,
+                            quarter_I=profit_1,
+                            quarter_II=profit_2,
+                            quarter_III=profit_3,
+                            quarter_IV=profit_4,
+                            profit_sum=profit)
+        companies.append(company_i)
+    avg_year = profit_all / n
+    return companies, avg_year
+
+
+def print_company(companies, avg_year):
+    more_avg = []
+    less_avg = []
+    for company in companies:
+        if company[5] > avg_year:
+            more_avg.append(company[0])
+        elif company[5] < avg_year:
+            less_avg.append(company[0])
+    print(f"Прибыль выше среднего {' '.join(more_avg)}")
+    print(f"Прибыль ниже среднего {' '.join(less_avg)}")
+
+
+def main():
+    try:
+        companies, avg_year = get_avg()
+        print_company(companies, avg_year)
+    except ValueError:
+        print('Неверный тип данных')
+
+
+if __name__ == '__main__':
+    main()
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,82 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+import collections
+
+
+def get_result():
+    input_num = input('Введите два числа в шестнадцатеричном формате через пробел: ').split()
+    numbers = collections.defaultdict(list)
+    for num in input_num:
+        numbers[num] = list(num)
+    sum_num = 0
+    mul_num = 1
+    for num in numbers.keys():
+        num = int(num, 16)
+        sum_num += num
+        mul_num *= num
+    sum_num = list(hex(sum_num).upper())[2:]
+    mul_num = list(hex(mul_num).upper())[2:]
+    return sum_num, mul_num
+
+
+def main():
+    try:
+        sum_num, mul_num = get_result()
+        print(f'Сумма чисел {sum_num}, поизведение {mul_num}')
+    except ValueError:
+        print('Неверный формат')
+
+
+# OR
+
+
+class OperationsNum:
+    def __init__(self, num_1, num_2):
+        self.num_first = num_1
+        self.num_second = num_2
+
+    def numbers_sum(self):
+        return list(hex(int(self.num_first, 16) + int(self.num_second, 16)).upper())[2:]
+
+    def numbers_mul(self):
+        return list(hex(int(self.num_first, 16) * int(self.num_second, 16)).upper())[2:]
+
+
+def main_1():
+    try:
+        num_1, num_2 = input('Введите два числа в шестнадцатеричном формате через пробел: ').split()
+        result = OperationsNum(num_1, num_2)
+        print(f'Сумма чисел {result.numbers_sum()}, поизведение {result.numbers_mul()}')
+    except ValueError:
+        print('Неверный формат')
+
+
+# OR
+
+
+class OperationsNumUpd:
+    def __init__(self, num_1, num_2):
+        self.num_first = num_1
+        self.num_second = num_2
+
+    def __add__(self, other):
+        return list(hex(int(self.num_first, 16) + int(self.num_second, 16)).upper())[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(self.num_first, 16) * int(self.num_second, 16)).upper())[2:]
+
+
+def main_2():
+    try:
+        num_1, num_2 = input('Введите два числа в шестнадцатеричном формате через пробел: ').split()
+        result = OperationsNumUpd(num_1, num_2)
+        print(f'Сумма чисел {result + result}, поизведение {result * result}')
+    except ValueError:
+        print('Неверный формат')
+
+
+if __name__ == '__main__':
+    main()
+    main_1()
+    main_2()
