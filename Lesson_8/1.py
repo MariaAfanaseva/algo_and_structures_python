6ca277d974319e52790f3024e1aa06445207c7e4
Maria Afanaseva <mashenkachukina@gmail.com>
2019-08-28 11:06:34 +0200

    Hufman, hash

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,50 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+
+
+def counting_frequency_letters(string):
+    letters = {}
+    for letter in string:
+        if letter not in letters:
+            letters[letter] = 1
+        else:
+            letters[letter] += 1
+    return letters
+
+
+def huffman_coding(letters):
+    frequency = list(letters.items())
+    for letter in letters:
+        letters[letter] = ''
+
+    while len(frequency) != 1:
+        # print(frequency)
+        min1 = min(frequency, key=lambda x: x[1])
+        for i in min1[0]:
+            letters[i] = '0{0}'.format(letters[i])
+        frequency.pop(frequency.index(min1))
+
+        min2 = min(frequency, key=lambda x: x[1])
+        for i in min2[0]:
+            letters[i] = '1{0}'.format(letters[i])
+        frequency.pop((frequency.index(min2)))
+        frequency.append((min1[0] + min2[0], min1[1] + min2[1]))
+        # print(min1, min2)
+        # print(frequency)
+        # print(letters)
+    return letters
+
+
+def result_print():
+    string = input('Введите строку: ')
+    letters = huffman_coding(counting_frequency_letters(string))
+    if len(letters) == 1:
+        letters[string[0]] = '0'
+    for let, cod in letters.items():
+        print(let + ': ' + cod)
+    for let in string:
+        print(letters[let], end='')
+
+
+result_print()
--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,21 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+
+import hashlib
+
+string = input("Введите строку из маленьких латинских букв: ")
+substrings = set()
+
+length = len(string)
+for i in range(length):
+    for j in range(i, length):
+        if i > 0 or i == j:
+            print(string[i:j+1])
+            substrings.add(hashlib.sha1(string[i:j+1].encode('utf-8')).hexdigest())
+        else:
+            print(string[i:j])
+            substrings.add(hashlib.sha1(string[i:j].encode('utf-8')).hexdigest())
+
+print(substrings)
+print(f"Количество различных подстрок в строке '{string}' равно {len(substrings)}")
